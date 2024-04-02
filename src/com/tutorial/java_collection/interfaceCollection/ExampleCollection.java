1238924b80f78ddab7cfe2ac34ca8d341bd1ce89
Ahmad Fairuz Ulumuddin <usleunmdi32@gmail.com>
2024-04-02 10:20:49 +0700

    [ADD]membuat collection

---
+++ src/com/tutorial/java_collection/interfaceCollection/ExampleCollection.java
@@ -0,0 +1,43 @@
+package com.tutorial.java_collection.interfaceCollection;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class ExampleCollection {
+    public static void main(String[] args) {
+        /**
+         * add() -> digunakan untuk menambah data sesuai dengan tipe data yang ditentukan di awal
+         * remove() -> digunakan untuk menghapus data berdasarkan value
+         * contains() -> cek apakah data nya ada atau tidak
+         *
+         *  name[] -> seperti ini masih enggak bisa disini
+         *  dicollection tidak bisa mendapatkan index
+        */
+
+        Collection<String> names = new ArrayList<>();
+        names.add("Jhiro");
+        names.add("Ilham");
+        names.add("Sauqi");
+        names.add("Abun");
+        names.add("Luky");
+        System.out.println(names);
+        names.remove("Luky");
+        names.remove("Ilham");
+        System.out.println(names);
+        System.out.println(names.contains("Jhiro"));
+
+        Collection<String> newNames = new ArrayList<>();
+        newNames.add("Haqi");
+        newNames.add("Zarek");
+        names.addAll(newNames);
+
+        System.out.println(names);
+
+        System.out.println(names.size() + " size");
+
+        for (String name : names) {
+            System.out.println(name);
+        }
+    }
+
+}
